4969a8d3c14531c62a4baaa1860a8f94c67222c2
Ikuo Kirihara <equo.keeley@gmail.com>
2014-03-22 18:14:44 +0900

    Add Singleton, State

---
+++ GoF_DesignPatterns/State/Context.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoF_DesignPatterns.State
+{
+    public class Context
+    {
+        private IState state;
+        public IState State
+        {
+            get { return state; }
+        }
+
+        private void ChangeState(IState state)
+        {
+            this.state = state;
+        }
+        
+        public Context(IState state)
+        {
+            this.state = state;
+        }
+
+        public void Request()
+        {
+            state.Handle(this);
+        }
+    }
+}
